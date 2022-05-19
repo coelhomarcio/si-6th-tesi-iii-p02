367a5fe40479f7879d18175e957f47da801c1612
Marcio Coelho <marciocoelho@gmail.com>
2022-05-19 04:17:31 -0300

    Initial commit

---
+++ common/src/Common/Paragraphs.hs
@@ -0,0 +1,13 @@
+module Common.Paragraphs where
+
+pHome01 :: String
+pHome01 = "Lista de tarefas desenvolvida para a disciplina Tópicos Especiais em Sistemas para Internet III, ministrada pelo Prof. Ms. Alexandre Garcia de Oliveira."
+
+pHome02 :: String
+pHome02 = "Integrantes:"
+
+pHome03 :: String
+pHome03 = "Marcio Coelho"
+
+pHome04 :: String
+pHome04 = "Victhória Nunes"
