bacd38dc385607fc4c7a6cedf8daa3059bd22b97
Victhoria Nunes <victhoria.nunes@fatec.sp.gov.br>
2022-05-29 20:00:19 -0300

    Formulário adicionado

--- frontend/src/PageCreate.hs
+++ frontend/src/PageCreate.hs
@@ -8,9 +8,31 @@ import qualified Data.Text as T
 
 import Common.Paragraphs
 
-pageCreate :: DomBuilder t m => m ()
+pageCreate :: (DomBuilder t m, MonadHold t m, PostBuild t m) => m ()
 pageCreate = do
     elAttr "div" ("class" =: "main") $ do
         el "main" $ do
             el "h2" $ text $ T.pack h2Create
-            el "p" $ text "Create works!"
+            elAttr "form" ("action" =: "javascript:void(0);") $ do
+                el "fieldset" $ do
+                    el "legend" $ text "Adicionar"
+                    el "div" $ do
+                        el "label" $ text "Tarefa"
+                        cxtext
+
+
+
+click :: DomBuilder t m => m (Event t T.Text)
+click = do
+    t <- inputElement def
+    (evt, _) <- el' "button" (text "Submit")
+    return $ attachPromptlyDynWith const (_inputElement_value t) (domEvent Click evt)
+
+
+
+cxtext :: (DomBuilder t m, MonadHold t m, PostBuild t m) => m ()
+cxtext = do
+    evt <- click
+    resultado <- holdDyn "" evt
+    el "p" (dynText resultado)
+
